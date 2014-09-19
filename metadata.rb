5f97119bee6fea9f302b8eed5509fe5cbaa0de35
David Martin <david.martin@feedhenry.com>
2014-09-19 12:06:30 +0100

    Initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'gitlab-shell'
+maintainer       'FeedHenry'
+maintainer_email 'david.martin@feedhenry.com'
+license          'All rights reserved'
+description      'Installs/Configures gitlab-shell'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
---
+++ recipes/default.rb
@@ -0,0 +1,8 @@
+#
+# Cookbook Name:: gitlab-shell
+# Recipe:: default
+#
+# Copyright 2014, FeedHenry
+#
+# All rights reserved - Do Not Redistribute
+#
